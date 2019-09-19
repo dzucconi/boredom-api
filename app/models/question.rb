0e959cd9ac437e3eb2caeaf736d03d02ebd9e654
dzucconi <mail@damonzucconi.com>
2019-09-19 13:16:24 -0400

    Handle errors when crawling

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,6 +13,8 @@ class Question < ApplicationRecord
     return if crawled?
 
     response = Scraper.process_question(url)
+    return if response.nil?
+
     related_questions = response[:related].map { |related| self.class.find_or_create_by!(related) }
     self.related_question_ids = related_questions.pluck(:id)
     self.crawled = true
